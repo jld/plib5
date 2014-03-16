1b3a920f5e3f552bc7b47a4251b65e74542c8ca6
Jed Davis <jld@panix.com>
2014-03-16 16:53:25 -0700

    Switch to harmonic mean and log once.

--- mandel.f90
+++ mandel.f90
@@ -9,20 +9,24 @@ subroutine iter(c, n, a)
   do i = 1,n
      x = x * x + c
      if (x /= 0) then
-        a = a + log(x)
+        a = a + (1 / x)
      end if
      if (real(x * conjg(x)) > 4) then
-        a = a / i
+        if (a /= 0) then
+           a = log(i / a)
+        end if
         return
      end if
   end do
-  a = a / n
+  if (a /= 0) then
+     a = log(n / a)
+  end if
 end subroutine iter
 
 
 program mandel
   implicit none
-  integer :: iters = 128
+  integer :: iters = 1024
   integer :: scale = 256
   integer :: yi, xi, gs, n
   complex*16 :: c, a
@@ -39,7 +43,7 @@ program mandel
         c = cmplx(x, y)
         call iter(c, n, a)
 !        write (*,*) "# c = ", c, ", n = ", n
-        gs = modulo(real(a) * 4 + imag(a) / tau, 1.0) * 256
+        gs = modulo(real(a) * 1.5 + imag(a) / tau, 1.0) * 256
         write (*,*) gs
      end do
   end do
